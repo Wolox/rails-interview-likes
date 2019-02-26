c79b6ee5a1a7ebe76a844d3de667f1f1f63ff98d
Nicolas Zarewsky <nzarewsky@gmail.com>
2019-02-26 12:16:52 -0300

    Fix title typo

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20190226150307) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "tile"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
