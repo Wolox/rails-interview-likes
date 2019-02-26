b4c8f19410946e0d4ee027f9a4c63b5919aba928
Nicolas Zarewsky <nzarewsky@gmail.com>
2019-02-26 12:05:11 -0300

    Add association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222130549) do
+ActiveRecord::Schema.define(version: 20190226150307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20190222130549) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 20190222130549) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
